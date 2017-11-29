1d7bc2242b9902011096d4f8b0c3738d635aefe6
19920625lsg <1648266192@qq.com>
2017-11-29 21:34:54 +0800

    后端项目首次提交

---
+++ src/main/java/com/huawei/l00379880/myblogbackend/MyblogBackendApplication.java
@@ -0,0 +1,12 @@
+package com.huawei.l00379880.myblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogBackendApplication.class, args);
+	}
+}
