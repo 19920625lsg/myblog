65c6825748acce12ca16386e17435345635c03f6
liang shan guang <1648266192@qq.com>
2017-12-17 11:10:09 +0800

    分类管理已经全部完成

--- src/main/java/com/huawei/l00379880/myblogbackend/controller/admin/TypeController.java
+++ src/main/java/com/huawei/l00379880/myblogbackend/controller/admin/TypeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -66,4 +67,41 @@ public class TypeController {
         }
         return "redirect:/admin/types";
     }
+
+    @PostMapping("/typeEdit/{id}")
+    public String typeAddPost(@Valid Type type, BindingResult result, @PathVariable Long id, RedirectAttributes attributes) {
+        // 校验分类是不是已经有了
+        Type tt = typeService.getTypeByName(type.getName());
+        if (tt != null) {
+            result.rejectValue("name", "nameError", "博客分类已存在！！");
+        }
+        // 出现异常直接返回添加页面
+        if (result.hasErrors()) {
+            return "admin/type-add";
+        }
+        // 根据id更新分类
+        Type t = typeService.updateType(id, type);
+        if (t == null) {
+            // 未保存成功,传给前端一个提示
+            attributes.addFlashAttribute("message", "修改博客分类失败！");
+        } else {
+            // 保存成功,传给前端一个提示
+            attributes.addFlashAttribute("message", "修改博客分类成功！");
+        }
+        return "redirect:/admin/types";
+    }
+
+    @GetMapping("/type/{id}/edit")
+    public String editType(@PathVariable Long id, Model model) {
+        model.addAttribute("type", typeService.getType(id));
+        return "admin/type-add";
+    }
+
+    @GetMapping("/type/{id}/delete")
+    public String deleteType(@PathVariable Long id, RedirectAttributes attributes) {
+        typeService.deleteType(id);
+        attributes.addFlashAttribute("message", "删除博客分类成功！");
+        return "redirect:/admin/types";
+    }
+
 }
